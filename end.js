ab539172d0c9fc0be7fd250a7a21f9a86869fa80
Karthik Dulam <63772598+DKarthik2222@users.noreply.github.com>
2020-08-07 13:12:53 +0530

    Update end.js

--- end.js
+++ end.js
@@ -28,5 +28,5 @@ saveHighScore = (e) => {
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
 
-    window.location.assign('/');
+    window.location.assign('/quizmaking/index.html');
 }
